056e487e675cbbefa081bd70df1eb0805c340388
sedrubal <dev@sedrubal.de>
2016-11-14 03:46:57 +0100

    Add mattermost, firefox,faufablab apps

--- apk_schleuder/config.py
+++ apk_schleuder/config.py
@@ -53,6 +53,17 @@ def get_whatsapp_version(soup):
     return versions[0].text.strip().lower().split(' ')[1]
 
 
+def get_firefox_apk_url(_):
+    """Return the static download url to firefox APK."""
+    return 'https://download.mozilla.org/?product=fennec-latest&os=android&lang=en-US'
+
+
+def get_firefox_version(soup):
+    """Return the version string found in HTML BeautifulSoup soup."""
+    return soup.select('html')[0].attrs['data-latest-firefox'].strip().lower()
+
+
+
 SOURCES = {
     'wire': {
         'wire.com': {
@@ -80,9 +91,26 @@ SOURCES = {
                 'get_apk_url': get_apk_href,
                 'get_apk_version': get_whatsapp_version,
                 'apk_signature_fingerprints': {
-                    'SHA256': '39:87:D0:43:D1:0A:EF:AF:5A:87:10:B3:67:14:18:FE:57:E0:E1:9B:65:3C:9D:F8:25:58:FE:B5:FF:CE:5D:44',
+                    'SHA256': \
+'39:87:D0:43:D1:0A:EF:AF:5A:87:10:B3:67:14:18:FE:57:E0:E1:9B:65:3C:9D:F8:25:58:FE:B5:FF:CE:5D:44',
                     'SHA1': '38:A0:F7:D5:05:FE:18:FE:C6:4F:BF:34:3E:CA:AA:F3:10:DB:D7:99',
                     'MD5': '55:6C:60:19:24:9B:BC:0C:AB:70:49:51:78:D3:A9:D1',
+                },
+            },
+        },
+    },
+    'firefox': {
+        'mozilla.org': {
+            'type': 'web',
+            'config': {
+                'url': 'https://www.mozilla.org/en-US/firefox/android/all/',
+                'get_apk_url': get_firefox_apk_url,
+                'get_apk_version': get_firefox_version,
+                'apk_signature_fingerprints': {
+                    'SHA256': \
+'A7:8B:62:A5:16:5B:44:94:B2:FE:AD:9E:76:A2:80:D2:2D:93:7F:EE:62:51:AE:CE:59:94:46:B2:EA:31:9B:04',
+                    'SHA1': '92:0F:48:76:A6:A5:7B:4A:6A:2F:4C:CA:F6:5F:7D:29:CE:26:FF:2C',
+                    'MD5': 'B1:E1:BC:EE:27:33:02:5E:CE:94:56:E4:19:A8:14:A3',
                 },
             },
         },
@@ -92,9 +120,29 @@ SOURCES = {
             'type': 'github',
             'config': {
                 'repo': 'FAU-Inf2/fablab-android',
-            }
-        }
-    }
+                'apk_signature_fingerprints': {
+                    'SHA256': \
+'9E:E0:7F:90:F6:E3:BE:97:CD:AC:E8:5D:00:AE:8C:84:13:99:25:FB:A5:7C:0B:D2:8A:90:54:F8:37:10:E9:62',
+                    'SHA1': '70:FB:ED:80:C4:6C:B1:32:00:C7:30:81:27:69:5D:8D:AB:1C:69:8B',
+                    'MD5': 'CC:54:03:C3:4A:0D:05:02:0E:A0:49:1A:BD:AC:81:34',
+                },
+            },
+        },
+    },
+    'mattermost': {
+        'github.com': {
+            'type': 'github',
+            'config': {
+                'repo': 'mattermost/android',
+                'apk_signature_fingerprints': {
+                    'SHA256': \
+'B1:36:5A:3D:8E:ED:77:EC:1E:D8:43:F8:36:D1:20:46:9A:4F:01:45:96:39:4C:FC:CD:20:79:57:1F:AA:E5:8C',
+                    'SHA1': 'E0:98:D2:8D:54:DB:C2:27:EA:50:BD:B4:A2:97:53:70:AB:A3:12:A8',
+                    'MD5':  'A4:0C:4A:E4:DE:01:5B:A5:E7:31:12:B4:14:76:F6:ED',
+                },
+            },
+        },
+    },
 }
 
 SETTINGS = {
